94f0b3b4141c4c6de015d3b89a0d61fcefc98483
Dmitry <dimalyamin2004@mail.ru>
2021-03-10 17:58:28 +0300

    add registration

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect
-from forms.defence import DefenceForm
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
+from forms.user import RegisterForm
 import datetime
 
 app = Flask(__name__)
@@ -14,6 +14,35 @@ def jobs():
     session = db_session.create_session()
     jobs = session.query(Jobs).all()
     return render_template('jobs.html', title='Jobs', arr=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def main():
